d935b2324fd86af22179fef7b424d5908cd82dc7
Geroge Nasour <gnasour@gmail.com>
2017-06-09 00:09:02 -0700

    Exercise 1-10 completed

--- charIO.c
+++ charIO.c
@@ -2,8 +2,28 @@
 
 main(){
 int c;
-while((c = getchar()) != EOF){
-putchar(c);
+while ((c = getchar()) != EOF)
+{
+	if(c == '\t'){
+	//printf("\\t");
+	putchar('\\');
+	putchar('t');
+	}
+	else if(c == '\n'){
+	//printf("\\n");
+	putchar('\\');
+	putchar('n');
+	}
+	else if(c == '\b'){
+	//printf("\\b");
+	putchar('\\');
+	putchar('b');
+	}
+	else
+	putchar(c);
+	printf("\n");
+
+	
 }
 
 }
