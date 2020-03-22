d1ea27a32acca67d406bf6fbbbefeb740bbf47ec
gnasour <gnasour@gmail.com>
2020-03-22 12:50:02 -0700

    Updated C to F conversions to abstract it into a function

--- K_R_Ex/CH1/temp_C_to_F.c
+++ K_R_Ex/CH1/temp_C_to_F.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
 
-#define UPPER 300
-#define LOWER 0
-#define STEP 20
+#define UPPER 300 //Upper limit of Celsius 
+#define LOWER 0 //Lower limit of Celsius
+#define STEP 20 //Increment operand for Celsius
+
+
+void calculate_cel_to_far(){
+    
+    int celcius = LOWER;
+    for(celcius; celcius < UPPER; celcius += STEP){
+        printf("%3d\t%6.1f\n", celcius, (9.0/5)*celcius+32);
+    }
+
+}
 
 int main(){
-    
+    calculate_cel_to_far();
 }
