49826d7dc9d1a33af7eb18b7e6d9668c497122ee
gnasour <gnasour@gmail.com>
2020-03-23 19:44:40 -0700

    Initialized arrays for words and set up skeleton of the program

--- K_R_Ex/CH1/histogram_word_length.c
+++ K_R_Ex/CH1/histogram_word_length.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
 
+#define MAXLENGTH 1000 //Max length of 1000 characters and 1000 word lengths
+
+int word_length(char* word_lengths, int word_length_index_size){
+
+}
+
+void print_histogram(char* word_lengths, int word_length_index_size){
+
+}
+
 int main(){
-    
+    char word[MAXLENGTH];
+    char word_lengths[MAXLENGTH];
+    char letter;
+    int word_index_size = 0;
+    int word_length_index_size = 0;
+    while((letter = getchar())!= EOF){
+
+        if(letter == '\n'){
+
+        }
+    }
     return 0;
 }
