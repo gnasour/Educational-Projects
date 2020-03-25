8f7b8c4588701436d38298ec50b439400aa66042
U-GITDIR\GEORGE.NASSOUR <GEORGE.NASSOUR@ML-1LBM6Y2.zurich.com>
2020-03-25 13:46:34 -0700

    Removed unnecessary variables and functions. Added more logic to IO and counting algorithms

--- K_R_Ex/CH1/histogram_word_length.c
+++ K_R_Ex/CH1/histogram_word_length.c
@@ -1,26 +1,38 @@
 #include <stdio.h>
 
-#define MAXLENGTH 1000 //Max length of 1000 characters and 1000 word lengths
+#define MAXLENGTH 10 //Max length of 1000 characters and 1000 word lengths
 
-int word_length(char* word_lengths, int word_length_index_size){
 
-}
-
-void print_histogram(char* word_lengths, int word_length_index_size){
-
+void print_histogram(int* word_lengths, int word_lengths_index_size){
+  for(int i = 0; i < word_lengths_index_size; i++){
+    printf("%d ", word_lengths[i]);
+  }
 }
 
 int main(){
-    char word[MAXLENGTH];
-    char word_lengths[MAXLENGTH];
+
+    int word_lengths[MAXLENGTH];
     char letter;
-    int word_index_size = 0;
-    int word_length_index_size = 0;
+    int word_size = 0;
+    int word_lengths_index_size = 0;
+    printf("Start input \n");
     while((letter = getchar())!= EOF){
-
+      
         if(letter == '\n'){
-
-        }
+	  if(word_size > word_lengths_index_size)
+	    word_lengths_index_size = word_size;
+	  word_lengths[word_size]++;
+	  word_size = 0;
+	}else{
+	  if(word_size < MAXLENGTH){
+	    word_size++;
+	  }else{
+	    printf("Word is too long, please press enter and type a word that is less than %d letters long\n", MAXLENGTH);
+	  }
+	}
     }
+
+    print_histogram(word_lengths, word_lengths_index_size);
+
     return 0;
 }
