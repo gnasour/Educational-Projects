23cb162ffda268bde21b3c29204f15aba3ab01fd
gnasour <gnasour@gmail.com>
2020-03-26 18:47:08 -0700

    "Updated gitignore to ignore .project files and formatted temp_C_to_F and temp_header

--- K_R_Ex/CH1/temp_C_to_F.c
+++ K_R_Ex/CH1/temp_C_to_F.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 
-#define UPPER 300 //Upper limit of Celsius 
-#define LOWER 0 //Lower limit of Celsius
-#define STEP 20 //Increment operand for Celsius
+#define UPPER 300 //Upper limit of Celsius
+#define LOWER 0   //Lower limit of Celsius
+#define STEP 20   //Increment operand for Celsius
 
+void calculate_cel_to_far()
+{
 
-void calculate_cel_to_far(){
-    
     int celcius = LOWER;
-    for(celcius; celcius < UPPER; celcius += STEP){
-        printf("%3d\t%6.1f\n", celcius, (9.0/5)*celcius+32);
+    for (celcius; celcius < UPPER; celcius += STEP)
+    {
+        printf("%3d\t%6.1f\n", celcius, (9.0 / 5) * celcius + 32);
     }
-
 }
 
-int main(){
+int main()
+{
     calculate_cel_to_far();
 }
--- K_R_Ex/CH1/temp_header.c
+++ K_R_Ex/CH1/temp_header.c
@@ -7,37 +7,43 @@
 
 void calculateTemp(int);
 float tempFormula(int);
-void printTable(float*,int);
+void printTable(float *, int);
 
-int main(int argc, char**argv){
-  if(argc > 1)
+int main(int argc, char **argv)
+{
+  if (argc > 1)
     calculateTemp(atoi(argv[1]));
   else
     calculateTemp(DEFAULT_ROWS);
   return 0;
 }
 
-void calculateTemp(int print_rows){
-  float * temperatures = malloc(sizeof(float)*print_rows*2);
-  for(int i = 0; i < print_rows*2; i+=2){
-     temperatures[i] = (float)i*5;
-     temperatures[i+1] = tempFormula(i*5);
+void calculateTemp(int print_rows)
+{
+  float *temperatures = malloc(sizeof(float) * print_rows * 2);
+  for (int i = 0; i < print_rows * 2; i += 2)
+  {
+    temperatures[i] = (float)i * 5;
+    temperatures[i + 1] = tempFormula(i * 5);
   }
   printTable(temperatures, print_rows);
 }
 
-float tempFormula(int celcius){
-  return (float)((celcius*9/5.0)+32);
+float tempFormula(int celcius)
+{
+  return (float)((celcius * 9 / 5.0) + 32);
 }
 
-void printTable(float* temperatures, int tempLen){
+void printTable(float *temperatures, int tempLen)
+{
   printf("\n    Temperature\n");
   printf("==================\n");
   printf("|  C");
   printf("    |   F    |\n");
   printf("------------------\n");
-  for(int i = 0; i < tempLen*2; i+=2){
-    printf("|%6.2f | %6.2f |\n",temperatures[i],temperatures[i+1]);
+  for (int i = 0; i < tempLen * 2; i += 2)
+  {
+    printf("|%6.1f | %6.2f |\n", temperatures[i], temperatures[i + 1]);
   }
   free(temperatures);
 }
