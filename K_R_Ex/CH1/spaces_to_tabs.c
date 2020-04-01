44763235733ea44373e3a7950e34fbd7002b8ce0
gnasour <gnasour@gmail.com>
2020-04-01 01:37:53 -0700

    initial skeleton of exercise

---
+++ K_R_Ex/CH1/spaces_to_tabs.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+#define MAX_LINE 1000
+
+int main(){
+
+    char c;
+    char line[MAX_LINE];
+    int counter = 0;
+    while((c=getchar())!=EOF){
+        if(c!='\n'){
+            
+        }
+    }
+
+}
