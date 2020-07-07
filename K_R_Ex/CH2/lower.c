35a8390391cd01fdae7c4da26d6326e7e2ed7f5d
gnasour <gnasour@gmail.com>
2020-07-07 09:36:26 -0700

    Finished lower program

--- K_R_Ex/CH2/lower.c
+++ K_R_Ex/CH2/lower.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 
+#define MAX_LEN 256
+
 int main(){
 
-    
+    char sentence[MAX_LEN];
+    char c;
+    int i = 0;
+    for(i; (c=getchar())!=EOF; i++){
+        c = (c >= 65 && c <= 90) ? c+32 : c;
+        sentence[i] = c;
+    }
+    sentence[i+1] = '\0';
+    printf("%s", sentence);
 
     return 0;
 }
