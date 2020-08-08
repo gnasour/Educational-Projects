af79064a90811f80ec0be4ec166521b07cb7899f
gnasour <gnasour@gmail.com>
2020-08-08 11:19:32 -0700

    Moved logic to seperate function

--- K_R_Ex/CH3/escape_func.c
+++ K_R_Ex/CH3/escape_func.c
@@ -2,30 +2,38 @@
 
 #define MAX_SIZE 256
 
+void escape(char*);
+
 int main(){
 
   char s[MAX_SIZE];
-  for(int i = 0; i < MAX_SIZE-1 && (s[i]=getchar())!=EOF; i++);
-  s[MAX_SIZE-1] ='\0';
-  for(int j = 0; s[j] !='\0'; j++){
-    switch(s[j]){
-    case '\a':
-      printf("\\a");
-    break;
+  int i;
+  for(i = 0; i < MAX_SIZE-1 && (s[i]=getchar())!=EOF; i++);
+  s[i] ='\0';
+  escape(s);
   
-    case '\b':
-      printf("\\b");
-      break;
-  case '\n':
-    printf("\\n");
-    break;
-  case '\t':
-    printf("\\t");
-    break;
-    default:
-      printf("%c", s[j]);
-    }
-  }
 
   return 0;
+}
+
+void escape(char* s){
+
+  for(int j = 0; s[j] !='\0'; j++){
+    switch(s[j]){
+      case '\a':
+        printf("\\a");
+        break;
+      case '\b':
+        printf("\\b");
+        break;
+      case '\n':
+        printf("\\n");
+        break;
+      case '\t':
+        printf("\\t");
+        break;
+      default:
+        printf("%c", s[j]);
+    }
+  }
 }
