f03561b7e43d184ffc897e547b37a09dd909f908
gnasour <gnasour@gmail.com>
2020-08-14 02:28:06 -0700

    Started itoa exercise

---
+++ K_R_Ex/CH3/itoa.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    
+    exit(EXIT_SUCCESS);
+    return 0;
+}
+
+void itoa(int n, char s[]){
+    
+    
+    int i, sign;
+
+    if((sign = n) < 0){
+        n = ~n;
+    }
+
+}
