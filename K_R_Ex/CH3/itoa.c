b59f6c99a0707cae81ecdaef73eba3038ff64d08
gnasour <gnasour@gmail.com>
2020-08-14 22:44:30 -0700

    Finished itoa program

--- K_R_Ex/CH3/itoa.c
+++ K_R_Ex/CH3/itoa.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+void itoa_ex(int, char[]);
 
 int main(){
 
-    
+    srand(time(NULL));
+    int n = rand()%1000;
+    char* itoa = calloc(1000, sizeof(char));
+    itoa_ex(n, itoa);
+    printf("%s", itoa);
     exit(EXIT_SUCCESS);
     return 0;
 }
 
-void itoa(int n, char s[]){
+void itoa_ex(int n, char s[]){
     
     
     int i, sign;
@@ -16,5 +24,19 @@ void itoa(int n, char s[]){
     if((sign = n) < 0){
         n = ~n;
     }
+    
+    i = 0;
+    
+    do{
+        s[i++] = n % 10 + '0';
+    }while((n /= 10) > 0);
+
+    if(sign < 0){
+        s[i++] = '-';
+    }
+    
+    s[i] = '\0';
+
+    strrev(s);
 
 }
