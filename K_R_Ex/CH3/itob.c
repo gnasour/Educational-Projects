12d2d9a5ba8bc0e76e4bc64d8c98ccb6ff1b5526
Charlie Root <root@root.lan>
2020-08-25 01:43:33 -0700

    Finished itob.c

--- K_R_Ex/CH3/itob.c
+++ K_R_Ex/CH3/itob.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <string.h>
 
-
 void itob(int, char*, int);
 
 int main(){
-
-    return 0;
+  char string[100];
+  itob(8, string, 2);
+  printf("%s", string);
+  return 0;
 }
 
 void itob(int n, char* s, int b){
@@ -14,20 +15,21 @@ void itob(int n, char* s, int b){
     int i = 0;
     char sign = 0;
     
-    (if n < 0){
+    if(n < 0){
         n = ~n;
-        sign = 1;
+        sign = '1';
     }
 
     do{
-        s[i++] = n%b + '0';
+
+      s[i++] = n%b + '0';
         
-    }while((n/=b) > 0)
+    }while((n/=b) > 0);
     
     if(sign){
         s[i++] = '-';
     }
-
+    s[i++] = '\0';
     strrev(s);
 
 }
