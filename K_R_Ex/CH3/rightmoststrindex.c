ee3aebaa0a2b2abac58732b96201e2d135de638b
Charlie Root <root@root.lan>
2020-08-26 23:56:43 -0700

    finished right most strindex program

---
+++ K_R_Ex/CH3/rightmoststrindex.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX 1000
+
+int strindex(char*,char*);
+
+
+int main(){
+
+  char string[] = "This is a test string a test poppers";
+  printf("%d\n", rightmoststrindex(string, "string")); 
+  
+  return 0;
+  
+}
+
+int rightmoststrindex(char* string, const char* pattern){
+
+  int i;
+  int right_most_index;
+  int pattern_len = strlen(pattern);
+  int string_len = strlen(string);
+  for(i = string_len; i != -1; i--){
+    if(pattern[pattern_len-1] == string[i]){
+      int j;
+      i--;
+      for(j = pattern_len-2; j != -1 && i != -1; j--, i--){
+	if(pattern[j] != string[i]){
+	  break;
+	}else if(j == 0 && pattern[j] == string[i]){
+	  return i;
+	}
+      }
+    }
+  }
+	
+	
+	
+  
+  return -1;
+}
