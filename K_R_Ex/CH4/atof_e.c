8f2d6049ad72c166d71ce8e7fd7d04ac45ba9565
gnasour <gnasour@gmail.com>
2020-08-30 13:36:43 -0700

    Started on the modification of the polish calculator

--- K_R_Ex/CH4/atof_e.c
+++ K_R_Ex/CH4/atof_e.c
@@ -4,17 +4,17 @@
 double atof(char *);
 
 int main(){
-    char* string = "12.8\0";
-    printf("%f",atof(string));
+    char* string = "12.8564E2\0";
+    printf("%f\n",atof(string));
     return 0;
 }
 
 double atof(char* s){
-    
     int i;
     
     double value = 0;
     double power = 1;
+    int exponent = 0;
     for(i = 0; isdigit(s[i]); i++){
         value = value*10 + (s[i] - '0');
     }
@@ -23,8 +23,20 @@ double atof(char* s){
         i++;
     }
     
-    for(i; s[i] != '\0'; i++){
+    for(i; isdigit(s[i]); i++){
         value += (s[i]-'0')/(power*=10);
+    }
+
+    if(s[i] == 'e' || s[i] == 'E'){
+        i++;
+    }
+
+    for(i; isdigit(s[i]); i++){
+        exponent = exponent* 10 + (s[i] - '0');
+    }
+
+    for(exponent; exponent > 0; exponent--){
+        value*=10;
     }
 
     return value;
