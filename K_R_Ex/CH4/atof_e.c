49b1a007fbef8195721020100d832e3a27398d81
gnasour <gnasour@gmail.com>
2020-08-30 11:37:02 -0700

    Fixing AtoF

--- K_R_Ex/CH4/atof_e.c
+++ K_R_Ex/CH4/atof_e.c
@@ -1,11 +1,31 @@
 #include <stdio.h>
+#include <ctype.h>
+
+double atof(char *);
 
 int main(){
-
-
+    char* string = "12.8\0";
+    printf("%f",atof(string));
     return 0;
 }
 
 double atof(char* s){
     
+    int i;
+    
+    double value = 0;
+    double power = 1;
+    for(i = 0; isdigit(s[i]); i++){
+        value = value*10 + (s[i] - '0');
+    }
+    
+    if(s[i] == '.'){
+        i++;
+    }
+    
+    for(i; s[i] != '\0'; i++){
+        value += (s[i]-'0')/(power*=10);
+    }
+
+    return value;
 }
