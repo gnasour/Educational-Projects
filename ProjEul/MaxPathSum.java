d0d739265797864d399f4c8352c4997c7650f3ae
medsys <medsys@localhost.localdomain>
2020-09-07 10:37:04 -0700

    Finished MaxPathSum question

--- ProjEul/MaxPathSum.java
+++ ProjEul/MaxPathSum.java
@@ -1,60 +1,77 @@
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
-
-import java.util.ArrayList;
 import java.util.StringTokenizer;
-import java.lang.IndexOutOfBoundsException;
 
 class MaxPathSum{
 
    public static void main(String [] args){
+      
+      //Processing input file
       Scanner scanner = null;
-      ArrayList<int[]> numInput = new ArrayList<int[]>();
+
+      //Tokenize the strings in the text files
       StringTokenizer numLine;
+
+      //Array to hold the results of the dynamic algorithm
       int [][] dynamic_num = new int[15][15];
       
+      //Buffer the text file using Scanner
       try{
          scanner = new Scanner(new File("MaxPathSum.txt"));
       }catch(FileNotFoundException fnf){
 
       }
+
+      //Populate the dynamic array with the initial values
       int k = 0;
       while(scanner.hasNext()){
          
          numLine = new StringTokenizer(scanner.nextLine(), " ");
-         //int[] lineTok = new int[numLine.countTokens()];
-            for(int i = 0; i <= numLine.countTokens(); i++){
-               //lineTok[i] = Integer.parseInt(numLine.nextToken());
-               dynamic_num[k][i] = Integer.parseInt(numLine.nextToken());
-            }
+         int numOfTokens = numLine.countTokens();
+         for(int i = 0; i < numOfTokens; i++){
+            dynamic_num[k][i] = Integer.parseInt(numLine.nextToken());
+         }
+
           k++;
-         //numInput.add(lineTok);
+
       }
+
+      //Efficient use of resources
       scanner.close();
-      
-      //dynamic_num = new int[numInput.size()][numInput.size()];
-      
-      for(int i = 0; i < numInput.size(); i++){
-         //int [] lineNum = numInput.get(i);
-         for(int j = 0; j < numInput.get(i).length; j++){
+           
+      for(int i = 0; i < dynamic_num.length; i++){
+         for(int j = 0; j < dynamic_num[0].length && dynamic_num[i][j] != 0; j++){
             
             try{
             
                if(dynamic_num[i-1][j-1] > dynamic_num[i-1][j]){
-                  dynamic_num[i][j] += dynamic_num[i-1][j-1];
+
+                  dynamic_num[i][j] = dynamic_num[i-1][j-1] + dynamic_num[i][j];
+
                }else{
-                  dynamic_num[i][j] += dynamic_num[i-1][j];
+
+                  dynamic_num[i][j] = dynamic_num[i-1][j] + dynamic_num[i][j];
+
                }
-            }catch(IndexOutOfBoundsException iob){}
+            }catch(IndexOutOfBoundsException iob){
+   
+               if(i > 0)
+                  dynamic_num[i][j] = dynamic_num[i-1][j] + dynamic_num[i][j];
+
+            }
          
          }
          
       }   
 
       for(int i = 0; i < dynamic_num[0].length; i++){
+ 
          System.out.print(dynamic_num[dynamic_num.length-1][i] + " ");
+ 
       }
+
+      System.out.println();
 
    
    }
