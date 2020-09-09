64e54502f242f741ae74d7580d97bd9cb4960a9d
George <gnasour@gmail.com>
2020-09-09 00:52:14 -0700

    Continuing work on counting sundays

---
+++ ProjEul/CountSundays.java
@@ -0,0 +1,10 @@
+public class CountSundays {
+    
+    public static void main(String [] args){
+        int daysInYear = 365;
+        String[] days = {"S", "M", "T", "W", "Th", "F", "Sa"};
+        String[] oddMonths = {"April", "June", "September", "November"};
+
+    }
+
+}
