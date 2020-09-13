0b86bcc59b94cb49693341630b3c1d4929b54201
George <gnasour@gmail.com>
2020-09-13 00:51:29 -0700

    Finished recursive reverse solution

--- K_R_Ex/CH4/recur_reverse.c
+++ K_R_Ex/CH4/recur_reverse.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 #define LEN 512
 
@@ -9,11 +10,22 @@ int main(){
     char s[LEN] = "Hello, World!";
 
     recur_reverse(s);
-
+    printf("%s\n", s);
     return 0;
 }
 
-void recur_reverse(char[] s){
-    if(s[0]!= '\0')
-        recur_reverse
+void recur_reverse(char* s){
+    
+    static int string_pos = 0;
+    static int string_rev_pos = 0;
+
+    char c;
+    
+    if((c = s[string_pos++]) != '\0'){
+        recur_reverse(s);
+        s[string_rev_pos++] = c;
+    }
+
+    
+
 }
