21864f569040d23ff4b5b73e017c061da97e0771
George <gnasour@gmail.com>
2020-09-17 20:03:14 -0700

    Finished Name Scores program

--- ProjEul/CountSundays.java
+++ ProjEul/CountSundays.java
@@ -1,30 +1,19 @@
 public class CountSundays {
-    
 
-        static final int DAYS_IN_YEAR = 365;
-        static final int DAYS_IN_LEAP_YEAR = 366;
         static final int CENTURY = 100;
 
         static boolean leapYear = false;
-        static int month = 0;
         //Jan 1901 Starts on a Tuesday
         static int day = 2;
         static int numOfFirstSundays = 0;
 
-        static String[] days = {"S", "M", "T", "W", "Th", "F", "Sa"};
+
 
     public static void main(String [] args){
 
 
         for(int i = 1901; i < CENTURY+1901; i++){
-            if(i == 2000){
-                System.out.println("Here");
-            }
-            if((i % 100 == 0 && i % 400 == 0) || (i % 4 == 0)){
-                leapYear = true;
-            }else{
-                leapYear = false;
-            }
+            leapYear = (i % 100 == 0 && i % 400 == 0) || (i % 4 == 0);
             for(int j = 0; j < 12; j++){
                 int daysToIterate = 0;
                 switch(j){
---
+++ ProjEul/NameScores.java
@@ -0,0 +1,51 @@
+import java.util.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.lang.String;
+
+class NameScores{
+    public static void main(String [] args){
+        
+        Scanner scanner = null;
+        String fileName = "p022_names.txt";
+        PriorityQueue pq = new PriorityQueue(5000);
+        long result = 0;
+        Path file = FileSystems.getDefault().getPath(".", fileName);
+        try (InputStream in = Files.newInputStream(file);
+            BufferedReader reader =
+              new BufferedReader(new InputStreamReader(in))) {
+            String line = null;
+            StringTokenizer st = null;
+            while ((line = reader.readLine()) != null) {
+                st = new StringTokenizer(line, ",");
+            }
+            String[] nameList = new String[st.countTokens()];
+            for(int i = 0; i < nameList.length; i++){
+                if(st.hasMoreTokens()){
+                    nameList[i] = new String(st.nextToken().replace('\"', ' ').trim());
+                }
+            }
+            Arrays.sort(nameList);
+            int nameVal = 0;
+            for(int i = 0; i < nameList.length; i++){
+                for(int j = 0; j < nameList[i].length(); j++){
+                    nameVal += nameList[i].toLowerCase().charAt(j) - 'a' + 1;
+                }
+                result += (i+1) * nameVal;
+                nameVal = 0;
+            }
+            System.out.println(result);
+            } catch (IOException x) {
+            System.err.println(x);
+        }
+
+    }
+}
+
