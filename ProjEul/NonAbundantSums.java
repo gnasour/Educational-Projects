80666e692417728777d56c418967d7ec9671c160
George <gnasour@gmail.com>
2020-09-25 23:54:02 -0700

    Commiting to work on PC

--- ProjEul/NonAbundantSums.java
+++ ProjEul/NonAbundantSums.java
@@ -15,7 +15,9 @@ class NonAbundantSums{
             if(AmicableNumbers.findSum(AmicableNumbers.findDivisors(i)) > i){
                 setOfAbundantSum.add(i);
             }
-        }
+        }//                if(result == 4179871){
+//                    System.out.println("Here");
+//
         Integer [] setOfAbundantSumArray = new Integer[setOfAbundantSum.size()];
         setOfAbundantSum.toArray(setOfAbundantSumArray);
         Arrays.sort(setOfAbundantSumArray);
@@ -23,9 +25,7 @@ class NonAbundantSums{
         //setOfAbundantSumArray[setOfAbundantSumArray.length-1]=99999;
         for(int i = 25; i < MAX_ABUNDANT_SUM; i++){
             for(int j = 0; setOfAbundantSumArray[j] < i; j++){
-//                if(result == 4179871){
-//                    System.out.println("Here");
-//                }
+
                 if(setOfAbundantSum.contains(i-setOfAbundantSumArray[j])){
                     toAdd = false;
                     break;
@@ -35,6 +35,9 @@ class NonAbundantSums{
                 result += i;
             }else{
                 toAdd = true;
+            }
+            if(result == 4190128){
+                System.out.println("Here");
             }
         }
         timeEnd = System.currentTimeMillis();
