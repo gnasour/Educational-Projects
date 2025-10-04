559f0c3e05b3f9ef7944d569bd05c72808d930ee
George Nassour <gnasour@gmail.com>
2025-10-04 15:18:57 -0700

    Finished char char_freq_histogram program

--- K_R_Ex/CH1_2025/char_freq_histogram.c
+++ K_R_Ex/CH1_2025/char_freq_histogram.c
@@ -1,9 +1,34 @@
 #include <stdio.h>
 
-#define CHAR_SET_LEN 26     // ASCII specifies 26 English alphabet characters if we ignore case
+#define CHAR_SET_LEN 27     // Size of English alphabet + undef chars
 
 
 int main()
 {
-    
+    int c;
+
+    int char_freq[CHAR_SET_LEN];
+    while((c=getchar())!=EOF){
+        
+        if(c >= 'A' && c < 'Z'+1){
+            char_freq[c - 'A']++;
+        }
+        else if(c >= 'a' && c < 'z'+1){
+            char_freq[c-'a']++;
+        }else{
+            char_freq[26]++;
+        }
+    }
+
+    //  Print the histogram
+    printf("Word Length Histogram\n");
+    printf("----------------------\n");
+    for(int i = 0; i < CHAR_SET_LEN; i++){
+
+        printf("%d:\t", i+1);
+        for(int j = char_freq[i]; j > 0; j--){
+            putchar('*');
+        }
+        putchar('\n');
+    }
 }
