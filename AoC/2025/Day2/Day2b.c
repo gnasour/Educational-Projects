c86671ca4760f6dff05838e1dfbfd91e5e204a88
George Nassour <gnasour@gmail.com>
2025-10-21 19:52:52 -0700

    Modularizing code for second part of day 2

--- AoC/2025/Day2/Day2b.c
+++ AoC/2025/Day2/Day2b.c
@@ -2,6 +2,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Arguments: A line of data to tokenize into integers, delimited by spaces
+// Returns: a pointer to an array of positive integers. -1 is used to signal end of array
+int* data_tok(char* line){
+    
+    
+    // Array of int data to return
+    int* num_arr = (int*) malloc(10*sizeof(int));
+    // Index of array
+    int num_in_line = 0;
+
+    // Line of text to tokenize
+    char* line_tok = strtok(line, " ");
+
+    while(line_tok != NULL){
+        num_arr[num_in_line] = atoi(line_tok);
+        num_in_line++;
+        line_tok = strtok(NULL, " ");
+    }
+    
+    num_arr[num_in_line] = -1;
+
+    return num_arr;
+}
+
+// Function checks whether the two numbers in sequence are increasing or decreasing
+// Returns 1 if increasing and 0 if decreasing
+int check_monotonicity(int first, int next){
+    
+}
 
 int main()
 {
@@ -9,66 +38,24 @@ int main()
     FILE* day2data = fopen("Day2.txt", "r");
     size_t num_bytes = 0;
     char* line = 0;
-    
-    // Num of safe lines
+
+    // Num of safe lines 
     int safe_lines = 0;
 
-    // Array to hold ints
-    int* num_arr = (int*) malloc(10*sizeof(int));
+    // Array of ints holding a line of tokenized data
+    int* num_arr;
     
     while(getline(&line, &num_bytes, day2data) != -1){
         
-        int num_in_line = 0;
-        char* line_tok = strtok(line, " ");
-        while(line_tok != NULL){
-            
-            num_arr[num_in_line] = atoi(line_tok);
-            num_in_line++;
-            line_tok = strtok(NULL, " ");
+        num_arr = data_tok(line);
+        for(int i = 0; num_arr[i] != -1; i++){
+            printf("%d ", num_arr[i]);
         }
-
-        // Boolean to check monotonicity of data
-        int increasing = 0;
-        if((num_arr[0] - num_arr[1]) < 0 ){
-                increasing = 1;
-        }
-
-        // Boolean to check if a line is safe
-        int safe = 1;
-
-        // Check if a level has multiple bad values
-        int bad_level = 0;
-
-        for(int i = 0; i < num_in_line-1 && safe; i++){
-            if((num_arr[i] - num_arr[i+1]) > 0 && increasing == 1){
-                bad_level++;
-                if(bad_level >= 2){
-                    safe = 0;
-                    break;
-                }
-                
-            }
-            else if((num_arr[i] - num_arr[i+1]) < 0 && increasing == 0){
-                bad_level++;
-                if(bad_level >= 2){
-                    safe = 0;
-                    break;
-                }
-            }
-            int diff = abs(num_arr[i] - num_arr[i+1]);
-            if(diff < 1 || diff > 3){
-                safe = 0;
-            }
-        }
-        if(safe){
-            safe_lines++;
-        }
-        safe = 1;
+        
+        free(num_arr);
     }
-    free(num_arr);
+
     free(line);
 
     printf("%d\n", safe_lines);
-
-    
 }
