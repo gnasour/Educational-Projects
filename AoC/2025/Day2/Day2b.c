8c007ba8059f6ddd3bb0de43bb57d4919bbb7a3c
George Nassour <gnasour@gmail.com>
2025-10-27 14:39:41 -0700

    Continuing day2 solution

--- AoC/2025/Day2/Day2b.c
+++ AoC/2025/Day2/Day2b.c
@@ -27,9 +27,14 @@ int* data_tok(char* line){
 }
 
 // Function checks whether the two numbers in sequence are increasing or decreasing
-// Returns 1 if increasing and 0 if decreasing
+// Returns 2 if increasing, 1 if decreasing, and 0 if the same
 int check_monotonicity(int first, int next){
-    
+    if(first>next)
+        return 1;
+    else if(first < next)
+        return 2;
+    else
+        return 0;
 }
 
 int main()
@@ -39,19 +44,70 @@ int main()
     size_t num_bytes = 0;
     char* line = 0;
 
+
     // Num of safe lines 
     int safe_lines = 0;
 
     // Array of ints holding a line of tokenized data
     int* num_arr;
+
+    // Current line being checked
+    int line_num = 1;
     
     while(getline(&line, &num_bytes, day2data) != -1){
+
+        // Boolean to check if current line is safe
+        int safe = 1;
+
+        // Check if a line has a bad value and if its been forgiven once
+        int bad_val = 0;
+        
         
         num_arr = data_tok(line);
-        for(int i = 0; num_arr[i] != -1; i++){
-            printf("%d ", num_arr[i]);
+
+        // Counters to check amount of increasing and decreasing pairs
+        int increasing = 0, decreasing = 0;
+
+        for(int i = 0; num_arr[i+1] != -1; i++){
+            
+            int monotonicity = check_monotonicity(num_arr[i], num_arr[i+1]);
+            int diff = abs(num_arr[i] - num_arr[i+1]);
+
+            if(monotonicity == 0){
+                safe = 0;
+                break;
+            }
+            else if(monotonicity == 2){
+                increasing++;
+            }
+            else{
+                decreasing++;
+            }
+            if(increasing > 1 && decreasing > 1){
+                safe = 0;
+                break;
+            }
+            if(diff > 3){
+                bad_val++;
+            }
+
+
+            
+            if(bad_val > 1){
+                safe = 0;
+                break;
+            }
         }
+
+        // Print line number currently being checked
+        // printf(": %d\n", line_num);
+        // line_num++;
         
+        
+        if(safe){
+            safe_lines++;
+        }
+        safe = 1;
         free(num_arr);
     }
 
