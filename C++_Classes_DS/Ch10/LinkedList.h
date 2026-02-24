ed253e95ebb21f593e6805e024c2c7b0c0850409
George Nassour <gnasour@gmail.com>
2026-02-24 15:16:08 -0800

    Implementing LinkedList ADT

---
+++ C++_Classes_DS/Ch10/LinkedList.h
@@ -0,0 +1,54 @@
+#ifndef LINKED_LIST
+#define LINKED_LIST
+
+template <class T>
+struct Node
+{
+    T data;
+    Node<T> *next;
+};
+
+template <class T>
+class LinkedList
+{
+public:
+    //Constructors
+    LinkedList();
+    LinkedList(const LinkedList<T> & ap);
+    ~LinkedList();
+    LinkedList<T> & operator=(const LinkedList<T> & rval);
+
+    //Methods
+    void insert(const T & elem);
+    bool first(T & elem);
+    inline bool getNext(T & elem);
+    bool find(const T & elem);
+    bool retrieve(T & elem);
+    bool replace(const T & newElem);
+    bool remove(T & elem);
+    bool isEmpty() const;
+    void makeEmpty();
+
+private:
+    Node<T> *start;
+    Node<T> *current;
+    
+    inline void deepCopy(const LinkedList<T> & original);
+
+};
+
+template <class T>
+LinkedList<T>::LinkedList()
+{
+    start = new Node<T>;
+    start->next = nullptr;
+}
+
+template <class T>
+LinkedList<T>::LinkedList(const LinkedList<T> & ap)
+{
+    deepCopy(ap);
+}
+
+
+#endif
