f40d71147332efc5677c0f607930e11a4fac3d58
George Nassour <gnasour@gmail.com>
2026-04-15 12:40:41 -0700

    Improving calculator design and function

---
+++ Programming_Stroustrup/Ch7/calc_v2.cpp
@@ -0,0 +1,177 @@
+#include "calc_v2.h"
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <cmath>
+
+Token_Stream ts;
+double expression();
+double term();
+double primary();
+void clean_up_mess();
+std::vector<Token> tok;
+
+double expression()
+{
+    double left = term();
+    Token t = ts.get();
+    while (true)
+    {
+        switch (t.kind)
+        {
+        case '+':
+            left += term();
+            t = ts.get();
+            break;
+        case '-':
+            left -= term();
+            t = ts.get();
+            break;
+        default:
+            ts.putback(t);
+            return left;
+            break;
+        }
+    }
+}
+
+double term()
+{
+    double left = primary();
+    Token t = ts.get();
+    while (true)
+    {
+        switch (t.kind)
+        {
+        case '*':
+            left *= primary();
+            t = ts.get();
+            break;
+        case '/':
+        {
+            double d = primary();
+            if (d == 0)
+            {
+                std::cerr << "Error: Divide by 0." << std::endl;
+                std::exit(-1);
+            }
+            left /= primary();
+            t = ts.get();
+            break;
+        }
+        case '%':
+        {
+            double d = primary();
+            if (d == 0)
+            {
+                std::cout << "Error: Divide by 0" << std::endl;
+                std::exit(5);
+            }
+            left = fmod(left, d);
+            t = ts.get();
+            break;
+        }
+        default:
+            ts.putback(t);
+            return left;
+            break;
+        }
+    }
+}
+
+double primary()
+{
+    Token t = ts.get();
+    switch (t.kind)
+    {
+    case '(':
+    {
+        double d = expression();
+
+        t = ts.get();
+        if (t.kind != ')')
+        {
+            std::cerr << "Error: ) expected." << std::endl;
+            std::exit(-2);
+        }
+
+        return d;
+        break;
+    }
+    case number:
+        return t.value;
+        break;
+    case '+':
+        return +primary();
+        break;
+    case '-':
+        return -primary();
+        break;
+    default:
+        std::cout << "Error: primary expected." << std::endl;
+        std::exit(-3);
+        break;
+    }
+}
+
+void calculate()
+{
+    while (std::cin)
+    {
+        try
+        {
+            std::cout << prompt; // Standard prompt
+            Token t = ts.get();  // Grab token for evaluation
+
+            while (t.kind == print)
+            {
+                t = ts.get();
+            }
+            if (t.kind == quit)
+            {
+                std::cout << "User exit" << std::endl;
+                std::exit(0);
+            }
+
+            ts.putback(t);
+            std::cout << result << expression() << std::endl;
+        }
+        catch (std::exception &e)
+        {
+            std::cerr << e.what() << std::endl;
+            clean_up_mess();
+        }
+    }
+}
+
+void clean_up_mess()
+{
+    while (true)
+    {
+        Token t = ts.get();
+        if (t.kind == print)
+        {
+            break;
+        }
+    }
+}
+
+int main()
+{
+    try
+    {
+        calculate();
+    }
+    catch (std::exception &e)
+    {
+        std::cerr << e.what() << std::endl;
+        return 1;
+    }
+    catch (...)
+    {
+        std::cerr << "exception" << std::endl;
+        return 2;
+    }
+
+    return 0;
+}
---
+++ Programming_Stroustrup/Ch7/calc_v2.h
@@ -0,0 +1,98 @@
+#ifndef CALC_H
+#define CALC_H
+
+#include <iostream>
+
+const char number = '8';
+const char quit = 'q';
+const char print = ';';
+const std::string prompt = "> ";
+const std::string result = "= ";
+
+struct Token
+{
+    char kind;
+    double value;
+};
+
+class Token_Stream
+{
+public:
+    Token_Stream();
+    Token get();
+    void putback(Token t);
+    void ignore(char c);
+
+private:
+    Token buffer;
+    bool full{false};
+};
+
+Token_Stream::Token_Stream()
+{
+}
+
+void Token_Stream::putback(Token t)
+{
+    if (full)
+    {
+        std::cout << "Error: Token Stream buffer is full" << std::endl;
+        std::exit(-3);
+    }
+    buffer = t;
+    full = true;
+}
+
+Token Token_Stream::get()
+{
+    if (full)
+    {
+        full = false;
+        return buffer;
+    }
+
+    char ch;
+    std::cin >> ch;
+
+    switch (ch)
+    {
+    case print:
+    case quit:
+    case '(':
+    case ')':
+    case '+':
+    case '-':
+    case '*':
+    case '/':
+    case '%':
+        return Token{ch};
+        break;
+    case '.':
+    case '0':
+    case '1':
+    case '2':
+    case '3':
+    case '4':
+    case '5':
+    case '6':
+    case '7':
+    case '8':
+    case '9':
+    {
+        std::cin.putback(ch);
+        double val;
+        std::cin >> val;
+        return Token{number, val};
+        break;
+    }
+    default:
+        std::cerr << "Error: Bad Token" << std::endl;
+        std::exit(-4);
+        break;
+    }
+
+    // Should never happen
+    return Token{' '};
+}
+
+#endif
